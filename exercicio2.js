a7045b1c36dbed8e305b161a24dfb3ed352d5883
DanielaLourenco <lourencodaniela.s@gmail.com>
2019-04-24 20:31:17 -0300

    Finalizacao do exercicio 2

--- exercicio2.js
+++ exercicio2.js
@@ -1,11 +1,31 @@
 // Faça o exercício da galeria de imagens aqui
 // Este arquivo ainda não está incluído no arquivo HTML
 
-const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
+const servidorDasImagens = "https://fegemo.github.io/cefet-web/images/",
   todasAsImagens = [
-    'philae-parts.jpg',
-    'philae-rosetta.jpg',
-    'philae-separation.jpg',
-    'philae-67-picture.jpg',
-    'philae-collecting.jpg'
+    "philae-parts.jpg",
+    "philae-rosetta.jpg",
+    "philae-separation.jpg",
+    "philae-67-picture.jpg",
+    "philae-collecting.jpg"
   ];
+  
+let index = 0;
+
+document.querySelector("#anterior").addEventListener("click", function(e)
+{
+  index--;
+  if(index < 0) 
+    index = todasAsImagens.length - 1;
+  
+  document.querySelector("#slide").src = servidorDasImagens + todasAsImagens[index];
+});
+
+document.querySelector("#proximo").addEventListener("click", function(e) 
+{
+  index++;
+  if(index >= todasAsImagens.length)
+    index = 0;
+  
+  document.querySelector("#slide").src = servidorDasImagens + todasAsImagens[index];
+});
