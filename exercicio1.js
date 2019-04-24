390ffb7b3767f32464db6b6274362493dc36a7b7
DanielaLourenco <lourencodaniela.s@gmail.com>
2019-04-24 20:07:41 -0300

    Finalizacao do exercicio 1

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,18 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+
+let lstBotoes = document.querySelectorAll(".botao-expandir-retrair");
+
+for (let i = 0; i < lstBotoes.length; i++) 
+{
+  lstBotoes[i].addEventListener("click", function(e) 
+  {        
+    let botaoAtivado = e.currentTarget;	
+    let paragrafo = botaoAtivado.parentNode;
+    let simbolo = botaoAtivado.innerHTML;
+
+	botaoAtivado.innerHTML = (simbolo === "+" ? "-" : "+");	
+       
+	paragrafo.classList.toggle("expandido");
+  });
+};
